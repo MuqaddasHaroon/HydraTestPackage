7efe503cfceeaa07ae50f12f3044c3b3ba6ce15b
Muqaddas Haroon <muqaddas.haroon@spqr.agency>
2022-01-31 16:23:59 +0500

    added colour

--- Sources/HydraTestPackage/FiveStarRating.swift
+++ Sources/HydraTestPackage/FiveStarRating.swift
@@ -7,18 +7,21 @@
 
 import SwiftUI
 
-public struct RatingView: View {
+public struct FiveStarRating: View {
     
     @Binding var rating: Int?
     var max: Int = 5
+    var colour: Color = .red
     
     /// this module will help you in creating a 5 start rating for
     /// - Parameters:
     ///   - rating: this variable will help you monitor the current rating you have selected..
     ///   - maxStart: this is to show the maximum amount of variable.
-    public init(rating: Binding<Int?>, maxStart: Int) {
+
+    public init(rating: Binding<Int?>, maxStart: Int, color: Color) {
         self._rating = rating
         self.max = maxStart
+        self.colour = color
     }
     
     
@@ -26,12 +29,13 @@ public struct RatingView: View {
         HStack {
             ForEach(1..<(max + 1), id: \.self) { index in
                 Image(systemName: self.starType(index: index))
-                    .foregroundColor(Color.red).onTapGesture {
+                    .foregroundColor(colour).onTapGesture {
                         self.rating = index
                     }
             }
         }
     }
+    
     private func starType(index: Int) -> String {
            
            if let rating = self.rating {
