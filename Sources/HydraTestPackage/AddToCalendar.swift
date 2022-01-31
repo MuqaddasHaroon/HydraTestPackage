dd17ef82b60888623b07fef88d439a60ee618f8e
Muqaddas Haroon <muqaddas.haroon@spqr.agency>
2022-01-31 14:33:36 +0500

    Fixed Calendar

--- Sources/HydraTestPackage/AddToCalendar.swift
+++ Sources/HydraTestPackage/AddToCalendar.swift
@@ -12,9 +12,10 @@ public struct AddToCalendar: View {
     @State var isChecked: Bool = false
     @State var width: CGFloat = 24
     @State var height: CGFloat = 24
-    @State var title: String
+    @State var title: String = "Add To Calendar"
     @State var startDate: Date = Date()
     @State var endDate: Date = Date()
+    @State var note: String = "Adding a test note swift"
     
     /// This component wil help you add an event to your calendar.
     /// - Parameters:
@@ -24,40 +25,45 @@ public struct AddToCalendar: View {
     ///   - title: any Text you want to add with the calendar..
     ///   - startDate: starting date with data type Date (optional)
     ///   - endDate: starting date with data type Date. (optional)
-//    public init(isChecked: Bool, width: CGFloat = 24, height: CGFloat = 24, title: String, startDate: Date, endDate: Date) {
-//        self.isChecked = isChecked
-//        self.width = width
-//        self.height = height
-//        self.title = title
-//        self.startDate = startDate
-//        self.endDate = endDate
-//
-//    }
+    ///   - note: any additional note you want to add.
+    ///
+    public init(isChecked: Bool, width: CGFloat, height: CGFloat, title: String, startDate: Date, endDate: Date, note: String) {
+        self.isChecked = isChecked
+        self.width = width
+        self.height = height
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+        self.note = note
+
+    }
   
     public var body: some View {
       
         Button(action:{
             isChecked.toggle()
          if isChecked {
-            let eventStore = EKEventStore()
-            switch EKEventStore.authorizationStatus(for: .event) {
-               case .authorized:
-                   insertEvent(store: eventStore, title: title, startDate: startDate, endDate: endDate)
-                   case .denied:
-                       print("Access denied")
-                   case .notDetermined:
-                   // 3
-                       eventStore.requestAccess(to: .event, completion:
-                         {(granted: Bool, error: Error?) -> Void in
-                             if granted {
-                                 insertEvent(store: eventStore, title: title, startDate: startDate, endDate: endDate)
-                             } else {
-                                   print("Access denied")
-                             }
-                       })
-                       default:
-                           print("Case default")
-              }
+             let eventStore = EKEventStore()
+                    
+                // 2
+                switch EKEventStore.authorizationStatus(for: .event) {
+                case .authorized:
+                    insertEvent(store: eventStore)
+                    case .denied:
+                        print("Access denied")
+                    case .notDetermined:
+                    // 3
+                        eventStore.requestAccess(to: .event, completion:
+                          {(granted: Bool, error: Error?) -> Void in
+                              if granted {
+                                insertEvent(store: eventStore)
+                              } else {
+                                    print("Access denied")
+                              }
+                        })
+                        default:
+                            print("Case default")
+                }
         }
             
         }){
@@ -67,40 +73,21 @@ public struct AddToCalendar: View {
         }
         
     }
-    func insertEvent(store: EKEventStore, title: String, startDate: Date, endDate: Date) {
-        // 1
-        DispatchQueue.main.async {
-        let calendars = store.calendars(for: .event)
-            
-        for calendar in calendars {
-            // 2
-            if calendar.title == "Calendar" {
-                // 3
-                let startDate = startDate
-                // 2 hours
-               // let endDate = endDate
-                    
-                // 4
-                let event = EKEvent(eventStore: store)
-                event.calendar = calendar
-                    
-                event.title = title
-                event.startDate = startDate
-                event.endDate = endDate
-                    
-                // 5
-                do {
-                    try store.save(event, span: .thisEvent)
-                }
-                catch {
-                   print("Error saving event in calendar")
-                    
-                }
-             
-            }
+    func insertEvent(store: EKEventStore) {
+        let event:EKEvent = EKEvent(eventStore: store)
+      
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.notes = note
+        event.calendar = store.defaultCalendarForNewEvents
+        do {
+            try store.save(event, span: .thisEvent)
+        } catch let error as NSError {
+        print("failed to save event with error : \(error)")
         }
-        }
-    }
+        print("Saved Event")
+  }
 }
 
 
